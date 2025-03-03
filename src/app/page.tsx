930f440c7105a493096e481970ee62da1309d625
Ben <bbrowning040518@gmail.com>
2025-03-03 00:19:11 +0000

    updated naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Philosophy from "@/components/sections/home/Philosophy";
 import KidsStories from "@/components/sections/home/KidsStories";
 import HowWeTeach from "@/components/sections/home/HowWeTeach";
 import Goals from "@/components/sections/home/Goals";
-import Newsletter from "@/components/sections/home/NewsLetter";
+import Newsletter from "@/components/sections/home/Newsletter";
 
 export default function HomePage() {
   return (
