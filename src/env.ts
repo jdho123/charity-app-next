4ab883fd40b0cccb5117b747cfef482d888444f6
Ben <bbrowning040518@gmail.com>
2025-03-06 19:55:40 +0000

    try to fix the env issue

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-
-export const PROTOCOL = process.env.NODE_ENV === 'production' ? 'https' : 'http'
-export const HOST = process.env.VERCEL_URL || 'localhost:3000'
-export const BASE_URL = `${PROTOCOL}://${HOST}`
-export const API_URL = `${BASE_URL}/api`
+export const PROTOCOL = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+export const HOST =
+  process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL || 'localhost:3000';
+export const BASE_URL = `${PROTOCOL}://${HOST}`;
+export const API_URL = `${BASE_URL}/api`;
