5c46cc15ab43284240a1cff2c918675d2cf50b41
anton <anton.zolochevskiy@gmail.com>
2025-04-08 12:10:11 +0200

    Add kids link to diary along with hover animations

--- src/components/sections/home/KidsStories.tsx
+++ src/components/sections/home/KidsStories.tsx
@@ -5,6 +5,9 @@ import React from 'react';
 import DiarySection from './kid_stories/DiarySection';
 import GloriaTitle from '@/components/shared/GloriaTitle';
 
+import '@/app/styles/HoverImages.css';
+import Link from 'next/link';
+
 const KidsStories: React.FC = () => {
   return (
     <section className="relative overflow-hidden bg-white w-full">
@@ -47,18 +50,73 @@ const KidsStories: React.FC = () => {
               }}
             />
           </div>
-
-          {/* Kids Holding Hands Drawing - Full Width */}
           <div className="relative w-full">
+            {/* 5 transparent overlays for hover detection (each 20% wide) */}
+            <Link
+              href="/diary/sister_love"
+              className="hoversection s1 absolute top-0 left-[10%] w-[10%] h-full"
+            />
+            <Link
+              href="/diary/not_forgotten"
+              className="hoversection s2 absolute top-0 left-[30%] w-[10%] h-full"
+            />
+            <Link
+              href="/diary/transformation"
+              className="hoversection s3 absolute top-0 left-[40%] w-[10%] h-full"
+            />
+            <Link
+              href="/diary/seven_oranges"
+              className="hoversection s4 absolute top-0 left-[50%] w-[10%] h-full"
+            />
+            <Link
+              href="/diary/light_of_christmas"
+              className="hoversection s5 absolute top-0 left-[60%] w-[20%] h-full"
+            />
+
+            {/* Default image (visible initially) */}
             <Image
-              src="/images/kidsHoldingHandsDrawing.png"
+              src="/images/kids/default.png"
               alt="Kids holding hands"
               width={1200}
               height={300}
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
+              className="defaultBg w-full h-auto transition-opacity duration-300 ease-in-out"
+            />
+
+            {/* Hover images: each starts hidden, absolute stacked, same styling */}
+            <Image
+              src="/images/kids/0_aligned.png"
+              alt="Kids holding hands - 0 aligned"
+              width={1200}
+              height={300}
+              className="aligned0 absolute top-0 left-0 w-full h-auto transition-opacity duration-300 ease-in-out opacity-0"
+            />
+            <Image
+              src="/images/kids/1_aligned.png"
+              alt="Kids holding hands - 1 aligned"
+              width={1200}
+              height={300}
+              className="aligned1 absolute top-0 left-0 w-full h-auto transition-opacity duration-300 ease-in-out opacity-0"
+            />
+            <Image
+              src="/images/kids/2_aligned.png"
+              alt="Kids holding hands - 2 aligned"
+              width={1200}
+              height={300}
+              className="aligned2 absolute top-0 left-0 w-full h-auto transition-opacity duration-300 ease-in-out opacity-0"
+            />
+            <Image
+              src="/images/kids/3_aligned.png"
+              alt="Kids holding hands - 3 aligned"
+              width={1200}
+              height={300}
+              className="aligned3 absolute top-0 left-0 w-full h-auto transition-opacity duration-300 ease-in-out opacity-0"
+            />
+            <Image
+              src="/images/kids/4_aligned.png"
+              alt="Kids holding hands - 4 aligned"
+              width={1200}
+              height={300}
+              className="aligned4 absolute top-0 left-0 w-full h-auto transition-opacity duration-300 ease-in-out opacity-0"
             />
           </div>
 
